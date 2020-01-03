c78c27c6f14ff0b70d21cab1d24d7f8e8770a41f
Motoki Naito <motoki.developer@gmail.com>
2020-01-03 14:22:22 +0900

    adding redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 
+import CollectionsOverview from './components/collections-overview/collections-overview';
+
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <CollectionsOverview />
     </div>
   );
 };
---
+++ src/components/collections-overview/collections-overview.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface Props {}
+
+const CollectionsOverview: React.FC<Props> = () => {
+  return (
+    <div>
+      <h1>Collections Overview</h1>
+    </div>
+  );
+};
+
+export default CollectionsOverview;
